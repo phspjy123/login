a9f2b4ec83694ba2de23425df1610eec6531691e
phspjy123 <105132717+phspjy123@users.noreply.github.com>
2023-07-21 23:05:36 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,8 +8,26 @@ loginBtn.addEventListener("click",login);
 
 function login(){
    const req = {
-    id: id.value,
-    psword: psword.value,
+     id: id.value,
+     psword: psword.value,
    };
-   console.log(req);
+   
+   fetch("/login",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+   })
+   .then((res)=>res.json())
+   .then((res)=>{
+      if(res.success){
+        location.href="/";
+      } else {
+        alert(res.msg);
+      }
+   })
+   .catch((err)=>{
+     console.error("로그인 중 에러 발생");
+   })
 }
