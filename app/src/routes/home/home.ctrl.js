9080b34bf41131564431f47d2a90b671ece71918
phspjy123 <105132717+phspjy123@users.noreply.github.com>
2023-07-22 12:45:00 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,37 +1,23 @@
-"use strict"
+"use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
-    home: (req,res)=>{
+    home: (req, res)=>{
         res.render("home/index");
     },
     
-    login: (req,res)=>{
+    login: (req, res)=>{
         res.render("home/login");
     },
 };
 
 
 const process = {
-    login: (req,res) =>{
-        const id = req.body.id,
-          psword = req.body.psword;
-        
-        const users = UserStorage.getUsers("id","psword");
-        
-        const response ={};
-        if(users.id.includes(id)){
-           const idx = users.id.indexOf(id);
-           if (users.psword[idx]===psword){
-                response.success = true;
-                return res.json(response);
-           }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다."
-        return res.json(response);
+    login:(req, res) => {
+      const user = new User(req.body);
+      const response = user.login();
+      return res.json(response);
     },
 };
 
