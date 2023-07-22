f29968d09320cc855a0bcea21e38e9155ee9912a
phspjy123 <105132717+phspjy123@users.noreply.github.com>
2023-07-22 15:45:59 +0900

    회원가입 화면 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -3,13 +3,17 @@
 const User = require("../../models/User");
 
 const output = {
-    home: (req, res)=>{
+    home: (req, res) => {
         res.render("home/index");
     },
     
-    login: (req, res)=>{
+    login: (req, res) => {
         res.render("home/login");
     },
+
+    register: (req, res) => {
+        res.render("home/register");
+    }
 };
 
 
